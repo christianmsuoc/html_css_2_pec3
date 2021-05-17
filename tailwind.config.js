984a6309e1f638fb2e8a3f714ff1114a7e52da41
Christian <munhiz.christian@gmail.com>
2021-05-17 02:23:57 +0200

    Maquetación de home terminada

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,13 +16,16 @@ module.exports = {
         'footer-bg': "url('../images/themetalband.png')"
       },
       gridTemplateColumns: {
-        'headerCols': 'auto minmax(75px, 400px) auto minmax(150px, 600px) auto',
+        'headerCols': '1fr minmax(75px, 400px) 1fr minmax(150px, 600px) 1fr',
         'mainCols': 'minmax(250px, 550px)',
-        'footerCols': 'minmax(550px, 1600px)'
+        'footerCols': 'minmax(550px, 1600px)',
+        'footerNavbarCols': '1fr auto 1fr',
+        'footerLinksCols': 'auto auto auto auto',
+        'footerSocialCols': 'max-content max-content max-content'
       },
       gridTemplateRows:  {
         'mainRows': 'minmax(250px, 550px) auto auto',
-        'footerRows': '1fr 1fr 1fr'
+        'footerRows': '2fr 1fr'
       }
     },
     fontFamily: {
@@ -35,7 +38,9 @@ module.exports = {
       },
       tangerine: '#F18805FF',
       white: '#F0F0F0',
+      'white-dark': '#CACACA',
       charcoal: {
+        lighter: '#6b8f99',
         light: '#40575d',
         normal: '#36494E',
         dark: '#2c3b3f'
